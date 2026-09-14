9adaa0694b567519843221e9e3b1faf92da30cc0
abrogden <abrogden@hotmail.com>
2026-09-14 17:46:04 +0100

    Updating to showcase products more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 const deployBase = process.env.DEPLOY_BASE || "/";
 
@@ -6,4 +7,15 @@ export default defineConfig({
   site: "https://stayprivate.com",
   base: deployBase,
   output: "static",
+
+  integrations: [
+    sitemap({
+      namespaces: {
+        news: false,
+        xhtml: false,
+        image: false,
+        video: false,
+      },
+    }),
+  ],
 });
